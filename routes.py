91a869db6a25da5b37b502887a5f882530cb493b
Vyacheslav <nicegeaming@yandex.ru>
2025-10-02 00:06:33 +0700

    new fix

--- routes.py
+++ routes.py
@@ -482,16 +482,6 @@ def init_routes(app):
         
         return render_template('login.html')
     
-    @app.route('/debug_telegram')
-    def debug_telegram():
-        """Отладочная страница для Telegram Web App"""
-        return render_template('debug_telegram.html')
-    
-    @app.route('/test_telegram')
-    def test_telegram():
-        """Минимальный тест Telegram Web App (без layout)"""
-        return render_template('test_telegram.html')
-    
     @app.route('/demo_login')
     def demo_login():
         """Демо-вход для тестирования без Telegram"""
